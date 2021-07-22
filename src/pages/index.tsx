b8d35ce51e5e4b3c14333215b853f572231b4c2e
loekTheDreamer <luke.carelsen@me.com>
2021-07-22 14:42:11 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// Step 1: Import React
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
